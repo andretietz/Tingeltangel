279358525a217d4f8abcf56c733868daf5dc82d2
André Tietz <andre.tietz@gmail.com>
2021-01-25 21:23:42 +0100

    started rewriting disassembler

--- bookpen/src/main/kotlin/com/andretietz/audiopen/Main.kt
+++ bookpen/src/main/kotlin/com/andretietz/audiopen/Main.kt
@@ -1,11 +1,11 @@
 package com.andretietz.audiopen
 
-import com.andretietz.audiopen.core.Decoder
+import com.andretietz.audiopen.core.Disassembler
 import java.io.ByteArrayInputStream
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        Decoder().decode(ByteArrayInputStream(arrayOf(0x00, 0x00, 0x10, 0x00, 0x00).map { it.toByte() }.toByteArray()))
+        Disassembler().disassemble(ByteArrayInputStream(arrayOf(0x00, 0x00, 0x10, 0x00, 0x00).map { it.toByte() }.toByteArray()))
     }
 }
