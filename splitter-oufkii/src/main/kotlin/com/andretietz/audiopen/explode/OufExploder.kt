77e22ea1d754de7da5e864bfdbe491800b3727f5
André Tietz <andre.tietz@gmail.com>
2021-02-02 10:56:37 +0100

    extracting mp3s work

--- splitter-oufkii/src/main/kotlin/com/andretietz/audiopen/explode/OufExploder.kt
+++ splitter-oufkii/src/main/kotlin/com/andretietz/audiopen/explode/OufExploder.kt
@@ -17,14 +17,14 @@ class OufExploder(
     val accessFile = RandomAccessFile(file, "r")
 
     // HEADER
-    val indexTable: Long = accessFile.readInt().toLong()
+    val indexTable: Int = accessFile.readInt()
     accessFile.skipBytes(4)
     val smallestMediaId: Int = accessFile.readInt()
     accessFile.skipBytes(4)
     val numberOfItems: Int = accessFile.readInt()
     val bookId: Int = accessFile.readInt()
 
-    accessFile.seek(indexTable)
+    accessFile.seek(indexTable.toLong())
     return BookData(bookId, extractBookDataItems(bookId, numberOfItems, smallestMediaId, accessFile))
   }
 
@@ -36,100 +36,61 @@ class OufExploder(
     accessFile: RandomAccessFile
   ): List<BookDataItem> {
     val targetDir = File(cacheDir, "$bookId").also { it.mkdir() }
-    return (0..itemCount)
+    val map = (0 until itemCount)
       .map { id ->
         id + mediaId to IndexTableItem(
-          Foo.getPositionFromCode(accessFile.readInt(), id),
-          accessFile.readInt(),
-          accessFile.readInt()
+          position = getPositionFromCode(accessFile.readInt(), id).toLong(),
+          size = accessFile.readInt(),
+          type = accessFile.readInt()
         )
       }.toMap()
-      .map { (code, tableItem) ->
-        accessFile.seek(tableItem.position)
-        val buffer = ByteArray(tableItem.size)
-        accessFile.read(buffer)
-        when (tableItem.type) {
-          1 -> BookDataItem.MP3(code, File(targetDir, "$code.mp3")
-            .also { file ->
-              file.createNewFile()
-              file.writeBytes(buffer)
-            })
-          2 -> { // script
-            // TBD
-            BookDataItem.Script(code)
-          }
-          else -> {
-            // TODO: log error
-            throw Error()
-          }
+
+    return map.mapNotNull { (code, tableItem) ->
+      accessFile.seek(tableItem.position)
+      val buffer = ByteArray(tableItem.size)
+      accessFile.read(buffer)
+      when (tableItem.type) {
+        1 -> BookDataItem.MP3(code, File(targetDir, "$code.mp3")
+          .also { file ->
+            file.createNewFile()
+            file.writeBytes(buffer)
+          })
+        2 -> { // script
+          // TBD
+          BookDataItem.Script(code)
+        }
+        else -> {
+          // TODO: log error?
+          null
         }
       }
+    }
   }
 
   companion object {
-    /*
-    public static int getPositionInFileFromCode(int code, int n) {
-      if (((code & 255) != 0) | (n < 0)) {
-        return -1;
-      }
-      n--;
-      code = code >> 8;
-      int c = ((code >> 3) & 1) | (((code >> 4) & 1) << 1) | (((code >> 5) & 1) << 2) | (((code >> 7) & 1) << 3) | (((code >> 9) & 1) << 4);
-      code -= n * 26 - E[c];
-      return code << 8;
-    }
-     */
-    fun getPositionFromCode(code: Int, n: Int): Long {
-      if (((code and 0xFF) != 0) or (n < 0)) {
-        return -1
-      }
-      val co = code shr 8
-      // ((code >> 3) & 1) | (((code >> 4) & 1) << 1) | (((code >> 5) & 1) << 2) | (((code >> 7) & 1) << 3) | (((code >> 9) & 1) << 4);
-      val c = ((co shr 3) and 1) or
-          (((co shr 4) and 1) shl 1) or
-          (((co shr 5) and 1) shl 2) or
-          (((co shr 7) and 1) shl 3) or
-          (((co shr 9) and 1) shr 4)
-      val a = ((code - (((n - 1) * 26) - LOOKUP_TABLE[c])) shl 8).toLong()
-      if(a<0) {
-        println(a)
-      }
-      return a
+    fun getPositionFromCode(code: Int, n: Int): Int {
+      if (((code and 0xFF) != 0) || (n < 0)) return -1
+
+      val co = code shr 8 // 0x00F0 -> 0x000F
+      val c = co shr 3 and 1 or
+        (co shr 4 and 1 shl 1) or
+        (co shr 5 and 1 shl 2) or
+        (co shr 7 and 1 shl 3) or
+        (co shr 9 and 1 shl 4)
+      val a = co - (((n - 1) * 26) - LOOKUP_TABLE[c])
+
+      return a shl 8 // 0x000F -> 0x00F0
     }
 
+    /**
+     * I have no clue how this numbers come together. It just works. Thanks to Martin
+     */
     private val LOOKUP_TABLE = arrayOf(
-      578, 562, 546, 530, 514, 498, 482, 466, 322, 306, 290, 274, 258, 242, 226, 210, -446, -462, -478, -494, -510, -526, -542, -558, -702, -718, -734, -750, -766, -782, -798, -814
-//      0x232,
-//      0x222,
-//      0x212,
-//      0x202,
-//      0x1f2,
-//      0x1e2,
-//      0x1d2,
-//      0x142,
-//      0x132,
-//      0x122,
-//      0x112,
-//      0x102,
-//      0xf2,
-//      0xe2,
-//      0xd2,
-//      0xfffffe42,
-//      0xfffffe32,
-//      0xfffffe22,
-//      0xfffffe12,
-//      0xfffffe02,
-//      0xfffffdf2,
-//      0xfffffde2,
-//      0xfffffdd2,
-//      0xfffffd42,
-//      0xfffffd32,
-//      0xfffffd22,
-//      0xfffffd12,
-//      0xfffffd02,
-//      0xfffffcf2,
-//      0xfffffce2,
-//      0xfffffcd2
+      // @formatter:off
+      578, 562, 546, 530, 514, 498, 482, 466, 322, 306, 290,
+      274, 258, 242, 226, 210, -446, -462, -478, -494, -510,
+      -526, -542, -558, -702, -718, -734, -750, -766, -782, -798, -814
+      // @formatter:on
     )
   }
 
--- splitter-oufkii/src/test/java/com/andretietz/audiopen/explode/OufExploderTest.kt
+++ splitter-oufkii/src/test/java/com/andretietz/audiopen/explode/OufExploderTest.kt
@@ -1,19 +1,21 @@
 package com.andretietz.audiopen.explode
 
-import okhttp3.internal.toHexString
-import org.junit.Assert.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import java.io.File
 
 class OufExploderTest {
+
+
   @Test
-  fun asd() {
+  fun `Exploding an ouf file works`() {
     val target = File("src/test/resources/target").also { it.mkdirs() }
     val exploder = OufExploder(target)
-    val file = File("src/test/resources/dist", "05003_en.ouf")
+    val file = File("src/test/resources/dist", "08091_en.ouf")
 
+    val book = exploder.explode(file)
 
-
-    exploder.explode(file)
+    assertThat(book.id).isEqualTo(8091)
+    assertThat(book.data.size).isEqualTo(8)
   }
 }
